b3157423b4ccda73cfc92b7831ac17618ea65a30
Eyad <ahmedeyad84@gmail.com>
2025-05-06 14:02:58 +0300

    validations for bodies

--- src/author/dto/CreateAuthor.dto.ts
+++ src/author/dto/CreateAuthor.dto.ts
@@ -1,5 +1,16 @@
+import { IsEmail, IsNotEmpty, IsString, MaxLength, MinLength } from "class-validator"
+import { Author } from "src/models/author.model"
+import { IsUnique } from "src/validators/IsUnique.validator"
+
 export class CreateAuthorDto {
-    // need validation
+    @IsNotEmpty()
+    @IsEmail()
+    @IsUnique(Author)
     email: string
+
+    @IsNotEmpty()       
+    @IsString()
+    @MinLength(3)
+    @MaxLength(10)
     name: string
 }
--- src/user/dto/CreateUser.dto.ts
+++ src/user/dto/CreateUser.dto.ts
@@ -1,5 +1,20 @@
+import { Exclude } from "class-transformer";
+import { IsEmail, IsNotEmpty, MinLength, ValidateIf } from "class-validator";
+import { User } from "src/models/user.model";
+import { IsUnique } from "src/validators/IsUnique.validator";
+
 export class CreateUserDto {
-  // need validation
+  @IsNotEmpty()
+  @IsEmail()
+  @IsUnique(User)
   email: string;
+
+  @IsNotEmpty()
+  @MinLength(5)
+  password: string;
+}
+
+export class ResponseCreateUserDto {
+  @Exclude()
   password: string;
 }
